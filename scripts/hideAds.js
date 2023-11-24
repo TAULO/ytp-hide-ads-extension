15149be41c6c71ead0028936a1ae18ae4ea5e77e
THOMAS TAULO <thomas@sirenia.eu>
2023-11-24 14:32:41 +0100

    add playbackrate

--- scripts/hideAds.js
+++ scripts/hideAds.js
@@ -2,63 +2,66 @@ console.debug("Hello from, Hide YT Ad's!")
 
 let hasMuted = true
 
-function hide(node) {
+function hide(video) {
                         
     // skip ad
     document.querySelector(".ytp-ad-skip-button-modern.ytp-button")?.click()
+
+    if (video.playbackRate = 1) {
+        video.playbackRate = 16
+    }
     
     // mute ad
-    if (!node.muted) {
-        node.muted = true
+    if (!video.muted) {
+        video.muted = true
         hasMuted = true
     }
     
     // hide ad
-    if (node.style.display !== "none") {
-        node.style.display = "none"
+    if (video.style.display !== "none") {
+        video.style.display = "none"
     }
 }
 
-function show(node) {
-    if (node.muted && hasMuted) {
-        node.muted = false
+function show(video) {
+    if (video.playbackRate !== 1) {  
+        video.playbackRate = 1
+    }
+
+    if (video.muted && hasMuted) {
+        video.muted = false
         hasMuted = false
     }
 
-    if (node.style.display === "none") {
-        node.style.display = "block" // return to default
+    if (video.style.display === "none") {
+        video.style.display = "block" // return to default
     }
 }
 
-async function handleTimeUpdate(node) {
-    let { enabled } = await chrome.storage.sync.get("enabled")
-
+async function handleTimeUpdate(video, enabled) {
     if (enabled) {
         const hasAd = document.querySelector(".video-ads.ytp-ad-module").children.length > 0
         
         if (hasAd) {
-            hide(node)
+            hide(video)
         } else {
-            show(node)
+            show(video)
         }
     } else {
-        show(node)
+        show(video)
     }
 }
 
 let video = document.querySelector("#movie_player > div.html5-video-container > video")
 
 if (video) {
-    // if the video has been paused and played again the script stops
-    // however set interval seems to correct this
-    // video.addEventListener("timeupdate", async () => {
-    //     // video = document.querySelector("#movie_player > div.html5-video-container > video")
-    //     await handleTimeUpdate(video)
-    // })
-
     setInterval(async () => { 
-        video = document.querySelector("#movie_player > div.html5-video-container > video")
-        await handleTimeUpdate(video)
+        const { enabled } = await chrome.storage.sync.get("enabled")
+
+        if (enabled) {
+            video = document.querySelector("#movie_player > div.html5-video-container > video")
+            await handleTimeUpdate(video, enabled)
+        }
     }, 500)
 }
 
